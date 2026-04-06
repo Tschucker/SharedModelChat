a3a076cb925af1b6bf72273897f4cb173ae70308
Tom <tdsepsilon@gmail.com>
2026-04-05 19:31:14 -0700

    Initial Commit

---
+++ SharedModelChat/SharedModelChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedModelChatApp.swift
+//  SharedModelChat
+//
+//  Created by Tom Schucker on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SharedModelChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SharedModelChatTests/SharedModelChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SharedModelChatTests.swift
+//  SharedModelChatTests
+//
+//  Created by Tom Schucker on 4/5/26.
+//
+
+import Testing
+@testable import SharedModelChat
+
+struct SharedModelChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
